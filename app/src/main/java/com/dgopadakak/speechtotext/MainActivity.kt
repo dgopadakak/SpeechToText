99fa12cb9d1e996e2dcda4094c9d35f1784ad38b
dgopadakak <dgopadakakv3@gmail.com>
2024-04-26 10:44:35 +0300

    Initial commit

---
+++ app/src/main/java/com/dgopadakak/speechtotext/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.dgopadakak.speechtotext
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.dgopadakak.speechtotext.ui.theme.SpeechToTextTheme
+import java.util.Locale
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SpeechToTextTheme {
+                val text = remember { mutableStateOf("") }
+                val result = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+                        result->
+                    if (result.resultCode == Activity.RESULT_OK){
+                        val results = result.data?.getStringArrayListExtra(
+                            RecognizerIntent.EXTRA_RESULTS
+                        ) as ArrayList<String>
+
+                        text.value = results[0]
+                    }
+                }
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(8.dp),
+                    verticalArrangement = Arrangement.SpaceAround,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = text.value)
+                    Button(
+                        onClick = {
+                            text.value = ""
+                            try {
+                                val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+                                intent.putExtra(
+                                    RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                                    RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+                                )
+                                intent.putExtra(
+                                    RecognizerIntent.EXTRA_LANGUAGE,
+                                    Locale.getDefault()
+                                )
+                                intent.putExtra(RecognizerIntent.EXTRA_PROMPT,"Say something")
+                                result.launch(intent)
+                            }catch (e:Exception){
+                                e.printStackTrace()
+                            }
+                        }
+                    ) {
+                        Text(text = "Listen")
+                    }
+                }
+            }
+        }
+    }
+}
